dfbe467229e7fd349bc621b574917dd3bace634f
Dani Gutiérrez <Guty@MacBook-Pro-de-Jessica.local>
2022-11-28 12:28:38 -0600

    Plot y alertas

---
+++ MainPage/warningBaja.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>JavaScript Alert Box by PHP</title>
+<?php  
+    echo '<script type="text/javascript">';
+    echo ' alert("Sube la temperatura del agua")';
+    echo '</script>';
+    header("Location: http://192.168.100.147:90/IoT/MainPage/plot-sensor.php")
+?>
+</head>
+
+<body>
+</body>
+</html>
